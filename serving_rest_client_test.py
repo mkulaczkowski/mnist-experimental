ee3ac79cfa943a6d4bf3c2624e429e0d89185c61
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2019-03-29 18:46:57 +0100

    Added subplot generation to show image prediction

--- serving_rest_client_test.py
+++ serving_rest_client_test.py
@@ -1,5 +1,5 @@
 from random import randint
-
+import matplotlib.pyplot as plt
 import requests
 import tensorflow as tf
 
@@ -7,6 +7,15 @@ import tensorflow as tf
 image_index = randint(0, 9999)  # You may select anything up to 60,000
 
 test_image_data = x_test[image_index].tolist()
+
+fig = plt.figure()
+i = image_index
+plt.subplot(1,1,1)
+plt.tight_layout()
+plt.imshow(x_test[i], cmap='gray', interpolation='none')
+plt.xticks([])
+plt.yticks([])
+plt.show()
 
 vector = []
 for item in test_image_data:
